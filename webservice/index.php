57a53a11b1cfc9c07e2054abee50e87ee2d0ee42
marlonacademi <marlon.academi@gmail.com>
2015-03-18 19:38:24 -0300

    tirando access origin *

--- webservice/index.php
+++ webservice/index.php
@@ -25,7 +25,7 @@
 		echo json_encode($result->fetch(PDO::FETCH_ASSOC));
 	});
 
-	$app->get('/getTotal', function () use ($app){
+	$app->get('/gettotal', function () use ($app){
 		$con = getConn();
 		$sql = 'SELECT  o.amount, b.id as id_bracelet, o.id as id_order FROM sb_customers c LEFT JOIN sb_customer_bracelet cb ON ( c.id = cb.id_customer) LEFT JOIN sb_bracelets b ON (b.id = cb.id_bracelet) LEFT JOIN sb_orders o ON (o.id_customer = c.id) WHERE c.id = cb.id_customer AND (b.tag = \''.$app->request->get('param').'\' OR c.cpf = \''.$app->request->get('param').'\')';
 		$result = $con->query($sql);
